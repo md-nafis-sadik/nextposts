12ef4dab468b7b4f784356596ee0fc77803a1f52
md-nafis-sadik <sadikmdnafis@gmail.com>
2024-10-04 13:06:01 +0600

    Next

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      CLOUDINARY_CLOUD_NAME: 'diehcwg3j',
+      CLOUDINARY_API_KEY: '973726579231451',
+      CLOUDINARY_API_SECRET: '9JU-zf6cdiNzRvS6izP6Pg5lZcs',
+    },
+  };
+  
+  export default nextConfig;
+  
